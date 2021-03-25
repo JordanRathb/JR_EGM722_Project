81185915778548f2f7b1b93c99e86bf770606332
JordanRathb <80048069+JordanRathb@users.noreply.github.com>
2021-03-25 14:45:58 +0000

    Updated .gitignore and python script
    .gitignore was updated to ignore changes to .png files, and the python script was amended to output the new data files

--- EGM722Project/BaseScript.py
+++ EGM722Project/BaseScript.py
@@ -10,16 +10,18 @@ import matplotlib.pyplot as plt
 #general template/workflow/to-do list:
 
 #load the datasets
-AONB = gpd.read_file('data_files/AONB.shp')    #AONB
-NationalP = gpd.read_file('data_files/National_Parks_(December_2018)_Boundaries_GB_BGC.shp')    #NationalParks
-MSettlements = gpd.read_file('data_files/Major_Towns_and_Cities__December_2015__Boundaries.shp')    #MajorCitiesandTowns
-    #Rivers
-    #Roads(?)
+AONB = gpd.read_file('data_files/AONB_SE.shp') #AONB
+NationalP = gpd.read_file('data_files/NationalParks_SE.shp') #National Parks
+MSettlements = gpd.read_file('data_files/Settlements_SE.shp') #Major Cities and Towns
+PopDens = gpd.read_file('data_files/PopDens_SE.shp') #Population Density
+Watercourses = gpd.read_file('data_files/Watercourses_SE.shp')  #Rivers
 
-CRS = ccrs.UTM(30) #define crs
+PopDens = PopDens.to_crs(epsg=27700)
+CRS = ccrs.UTM(29) #define crs
+
 
 #figure creation
-Figure = plt.figure(figsize=(30, 30), edgecolor='k')    #establish figure dimensions with plt
+Figure = plt.figure(figsize=(30, 30))    #establish figure dimensions with plt
     #define a scale bar
 Axes = plt.axes(projection=ccrs.Mercator())    #define axes
     #define legend library
@@ -33,21 +35,22 @@ Axes = plt.axes(projection=ccrs.Mercator())    #define axes
     #...
 
 #to figure:
-AONB_features = ShapelyFeature(AONB['geometry'], CRS, edgecolor='k', facecolor='green')    #add NP and AONB
-NationalParks = ShapelyFeature(NationalP['geometry'], CRS, facecolor='darkseagreen')    #add new rivers
-CitiesAndTowns = ShapelyFeature(MSettlements['geometry'], CRS, edgecolor='k', facecolor='dimgrey')
-    #add settlement and road buffers
-    #...
-    #testing new branch
+AONB_features = ShapelyFeature(AONB['geometry'], CRS, edgecolor='k', facecolor='green') #add AONB
+NationalParks = ShapelyFeature(NationalP['geometry'], CRS, facecolor='darkseagreen') #add national parks
+CitiesAndTowns = ShapelyFeature(MSettlements['geometry'], CRS, edgecolor='k', facecolor='dimgrey') #add settlements
+PopulationDensity = ShapelyFeature(PopDens['geometry'], CRS, edgecolor='k', facecolor='lightsalmon') #add county polygons with popdens
+Water_courses = ShapelyFeature(Watercourses['geometry'], CRS, edgecolor='b') #add watercourses
 
-xmin, ymin, xmax, ymax = NationalP.total_bounds
+xmin, ymin, xmax, ymax = PopDens.total_bounds
 
 Axes.set_extent([xmin, xmax, ymin, ymax], crs=CRS)
+Axes.add_feature(CitiesAndTowns)
+Axes.add_feature(PopulationDensity)
+Axes.add_feature(Water_courses)
 Axes.add_feature(AONB_features)
 Axes.add_feature(NationalParks)
-Axes.add_feature(CitiesAndTowns)
 
-#output individual shapesfiles (if user wishes to manipulate further in GIS) -- may be redundant:
+#output individual shapesfiles (if user wishes to manipulate further in GIS):
     #AONB.shp
     #NationalParks.shp
     #MajorCitiesandTowns.shp
@@ -55,4 +58,4 @@ Axes.add_feature(CitiesAndTowns)
     #Roads.shp
 
 #output map figure of performed analysis in this script
-Figure.savefig('testfigure.png', dpi=300)    #output map.png
+Figure.savefig('Test.png', dpi=300)    #output map.png
