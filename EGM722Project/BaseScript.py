f652bd4e6d52fd8e9bdc3788b17198dd5c70d2b4
JordanRathb <80048069+JordanRathb@users.noreply.github.com>
2021-03-26 13:50:24 +0000

    Updated script to extract rivers
    Updated the script to select rivers that are not overlain with the settlement buffer zones

--- EGM722Project/BaseScript.py
+++ EGM722Project/BaseScript.py
@@ -1,13 +1,11 @@
 # import pandas as pd
 import geopandas as gpd
 from cartopy.feature import ShapelyFeature
-#from shapely.geometry import LineString, Polygon
+# from shapely.geometry import LineString, Polygon
 import cartopy.crs as ccrs
 # import matplotlib.patches as patches
 # import matplotlib.lines as lines
 import matplotlib.pyplot as plt
-
-# general template/workflow/to-do list:
 
 # load the datasets
 AONB = gpd.read_file('data_files/AONB_SE.shp')  # AONB
@@ -29,13 +27,22 @@ Axes = plt.axes(projection=ccrs.Mercator())  # define axes
 
 # analysis:
 
+# generate settlement buffers
 SettlementBuffer = MSettlements.buffer(10000)  # generate buffer around settlements with user defined distance
 SettlementBuffer = gpd.geoseries.GeoSeries([geom for geom in SettlementBuffer.unary_union.geoms])  # merge overlapping buffers to one
+SettlementBuffer_gdf = gpd.GeoDataFrame(gpd.GeoSeries(SettlementBuffer))
+SettlementBuffer_gdf = SettlementBuffer_gdf.rename(columns={0: 'geometry'}).set_geometry('geometry')
+SettlementBuffer_gdf.crs = 'epsg:27700'
+
+
 # extract rivers that are outside the buffer zones
+RiverExtractOutsideSettlement = gpd.overlay(Watercourses, SettlementBuffer_gdf, how='difference', keep_geom_type=False)  # select through difference where rivers do not overlay
+# the buffers around settlements
+
 # extract rivers that intersect with county polygons that are below a user defined threshold
 # generate buffer around river lines
 # clip county polygons to those that lay within the river buffers
-# output to figure
+
 
 # output elements to shapely features to be loaded into the figure:
 AONB_features = ShapelyFeature(AONB['geometry'], CRS, edgecolor='k', facecolor='green')  # add AONB
@@ -44,6 +51,7 @@ CitiesAndTowns = ShapelyFeature(MSettlements['geometry'], CRS, edgecolor='k', fa
 CitiesAndTownsBuffer = ShapelyFeature(SettlementBuffer, CRS, edgecolor='k', alpha=0.5)  # add settlement buffers
 PopulationDensity = ShapelyFeature(PopDens['geometry'], CRS, edgecolor='k', facecolor='lightsalmon')  # add county polygons with popdens
 Water_courses = ShapelyFeature(Watercourses['geometry'], CRS, edgecolor='b')  # add watercourses
+Water_coursesOS = ShapelyFeature(RiverExtractOutsideSettlement['geometry'], CRS, edgecolor='r')
 
 xmin, ymin, xmax, ymax = PopDens.total_bounds  # define the maximum extent of figure to the population density shapefile
 Axes.set_extent([xmin, xmax, ymin, ymax], crs=CRS)  # set the maximum extent of figure to the population density shapefile
@@ -52,11 +60,12 @@ Axes.set_extent([xmin, xmax, ymin, ymax], crs=CRS)  # set the maximum extent of 
 Axes.add_feature(PopulationDensity)
 Axes.add_feature(CitiesAndTowns)
 Axes.add_feature(Water_courses)
+Axes.add_feature(Water_coursesOS)
 Axes.add_feature(AONB_features)
 Axes.add_feature(NationalParks)
 Axes.add_feature(CitiesAndTownsBuffer)
 
-# output individual shapesfiles (if user wishes to manipulate further in GIS):
+# output individual shapefiles (if user wishes to manipulate further in GIS):
 # AONB.shp
 # NationalParks.shp
 # MajorCitiesandTowns.shp
